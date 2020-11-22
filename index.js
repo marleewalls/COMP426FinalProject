ae69315be38b1e6e3a42289590aa8aeb5ac0b8bb
margreen94 <margreen@margreen-pc.local>
2020-11-22 17:41:04 -0500

    add browsersync something

--- index.js
+++ index.js
@@ -254,6 +254,20 @@ app.get('/', function (req, res) {
     res.sendFile(path.join('./index.html'))
 })
 
+app.get('browserSync', () => {
+    browserSync.init({
+        server: {
+            baseDir: task.dir.base,
+            middleware: [
+                webpackDevMiddleware(bundler, {
+                    publicPath: webpackConfig.output.publicPath,
+                    stats: 'errors-only'
+                })
+            ]
+        },
+        browser: 'chrome'
+    });
+});
 
 
 const port = 3030;
