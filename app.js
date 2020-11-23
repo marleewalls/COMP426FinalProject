cc7125a6009a037e2552a96fa382bb72f07b22b2
margreen94 <margreen@margreen-pc.local>
2020-11-22 23:16:04 -0500

    app.js

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get("/", function (req, res) {
 
 
 const port = 3000;
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("It worked!");
 });
