de39657a3b41d331b9daeff9afda6ae3f83b3c41
margreen94 <margreen@margreen-pc.local>
2020-11-22 23:04:34 -0500

    ready to deploy

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
-function initMap() {
-    // Create the map.
-    const map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: { lat: 35.913255, lng: -79.055855 },
-    });
+const express = require('express');
+const app = express();
 
-    // Load the stores GeoJSON onto the map.
-    map.data.loadGeoJson('stores.json', { idPropertyName: 'storeid' });
+app.use(express.static("public"));
 
-    const apiKey = 'AIzaSyCExODL27Nhptizxz_ReP0XDWuiUNzeN9A';
-    const infoWindow = new google.maps.InfoWindow();
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World!</h1>");
+})
 
-    // Show the information for a store when its marker is clicked.
-    map.data.addListener('click', (event) => {
-        const category = event.feature.getProperty('category');
-        const name = event.feature.getProperty('name');
-        const description = event.feature.getProperty('description');
-        const hours = event.feature.getProperty('hours');
-        const phone = event.feature.getProperty('phone');
-        const position = event.feature.getGeometry().get();
-        const content = `
-        <h2>${name}</h2><p>${description}</p>
-        <p><b>Open:</b> ${hours}<br/><b>Phone:</b> ${phone}</p>
-      `;
 
-        infoWindow.setContent(content);
-        infoWindow.setPosition(position);
-        infoWindow.setOptions({ pixelOffset: new google.maps.Size(0, -30) });
-        infoWindow.open(map);
-    });
-}
+const port = 3000;
+app.listen(process.env.PORT || 3000, () => {
+    console.log("It worked!");
+});
