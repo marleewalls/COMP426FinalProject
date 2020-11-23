066b0426cd5052adbb8cee7fba79c4c061d22855
margreen94 <margreen@margreen-pc.local>
2020-11-22 23:23:00 -0500

    after local host

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
+var path = require('path');
 
-app.use(express.static("public"));
+app.use(express.static("/Users/margreen/COMP426FinalProject"));
 
 app.get("/", function (req, res) {
-    res.send("<h1>Hello World!</h1>");
+    // res.send("<h1>Hello World!</h1>");
+    res.sendFile(path.join('/Users/margreen/COMP426FinalProject/index.html'))
 })
 
 
-const port = 3000;
+// const port = 3000;
 app.listen(process.env.PORT || 5000, () => {
     console.log("It worked!");
 });
